e64d274e0d9135a07610113395efa3f0eefa8ffb
Genta Kaneyama <genta-kaneyama@cookpad.com>
2013-06-25 16:46:05 +0900

    fix escape method for japanese char

--- lib/rsolr/char.rb
+++ lib/rsolr/char.rb
@@ -3,19 +3,18 @@ module RSolr::Char
   
   # backslash everything
   # that isn't a word character
-  def escape value
-    value.gsub(/(\W)/, '\\\\\1')
-  end
-  
-  # LUCENE_CHAR_RX = /([\+\-\!\(\)\[\]\^\"\~\*\?\:\\]+)/
-  # LUCENE_WORD_RX = /(OR|AND|NOT)/
-  # 
-  # # More specific/lucene escape sequence
-  # def lucene_escape string
-  #   delim = " "
-  #   string.gsub(LUCENE_CHAR_RX, '\\\\\1').split(delim).map { |v|
-  #     v.gsub(LUCENE_WORD_RX, '\\\\\1')
-  #   }.join(delim)
+  # def escape value
+  #   value.gsub(/(\W)/, '\\\\\1')
   # end
   
+  LUCENE_CHAR_RX = /([\+\-\!\(\)\[\]\^\"\~\*\?\:\\]+)/
+  LUCENE_WORD_RX = /(OR|AND|NOT)/
+
+  # More specific/lucene escape sequence
+  def escape string
+    delim = " "
+    string.gsub(LUCENE_CHAR_RX, '\\\\\1').split(delim).map { |v|
+      v.gsub(LUCENE_WORD_RX, '\\\\\1')
+    }.join(delim)
+  end
 end
