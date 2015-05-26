5c5c5856bb156d30da8164b96ebd3d8cd97b632a
Genta Kaneyama <genta-kaneyama@cookpad.com>
2015-05-26 10:54:31 +0900

    Add patch for sunspot 2.2

--- lib/rsolr.rb
+++ lib/rsolr.rb
@@ -19,4 +19,17 @@ module RSolr
   # RSolr.escape
   extend Char
   
+  # from https://github.com/rsolr/rsolr/commit/266aedb37b01b95e021a5619e2649d17ddbe1e37
+  #
+  # backslash escape characters that have special meaning to Solr query parser
+  # per http://lucene.apache.org/core/4_0_0/queryparser/org/apache/lucene/queryparser/classic/package-summary.html#Escaping_Special_Characters
+  #  + - & | ! ( ) { } [ ] ^ " ~ * ? : \ /
+  # see also http://svn.apache.org/repos/asf/lucene/dev/tags/lucene_solr_4_9_1/solr/solrj/src/java/org/apache/solr/client/solrj/util/ClientUtils.java
+  #   escapeQueryChars method
+  # @return [String] str with special chars preceded by a backslash
+  def self.solr_escape(str)
+    # note that the gsub will parse the escaped backslashes, as will the ruby code sending the query to Solr
+    # so the result sent to Solr is ultimately a single backslash in front of the particular character
+    str.gsub(/([+\-&|!\(\)\{\}\[\]\^"~\*\?:\\\/])/, '\\\\\1')
+  end
 end
